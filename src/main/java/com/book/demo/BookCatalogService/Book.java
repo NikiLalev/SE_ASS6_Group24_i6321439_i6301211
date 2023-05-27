b314ea42f27529b1804373c6b26b1e1b44955a4b
Gabriel <eutu20032003@gmail.com>
2023-05-27 12:35:23 +0200

    tests

--- src/main/java/com/book/demo/BookCatalogService/Book.java
+++ src/main/java/com/book/demo/BookCatalogService/Book.java
@@ -7,11 +7,13 @@ import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@AllArgsConstructor
 public class Book {
 
     
--- src/test/java/com/book/demo/BookCatalogControllerTest.java
+++ src/test/java/com/book/demo/BookCatalogControllerTest.java
@@ -1,13 +1,100 @@
 package com.book.demo;
 
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.book.demo.BookCatalogService.Book;
+import com.book.demo.BookCatalogService.BookCatalogController;
+import com.book.demo.BookCatalogService.BookCatalogService;
+import com.book.demo.BookInventoryService.BookInvetoryService;
 
 public class BookCatalogControllerTest {
-    
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BookCatalogService bookCatalogService;
+
+    @Mock
+    private BookInvetoryService bookInventoryService;
+
+    @InjectMocks
+    private BookCatalogController bookCatalogController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(bookCatalogController).build();
+    }
+
     @Test
-    public void simpleRequestTest() {
-        // mockMvc.perform(get("/beanNameUrl"))
-        //   .andExpect(status().isOk())
-        //   .andExpect(view().name("welcome"));
+    public void testGetBookById() throws Exception {
+        // Arrange
+       int bookId = 1;
+        Book book = new Book(bookId, "1984", "George Orwell", 1949);
+
+        when(bookCatalogService.getByID(bookId)).thenReturn(book);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/catalog/book/{id}", bookId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(bookId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("George Orwell"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("1984"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.publicationYear").value(1949));
+            }       
+
+    @Test //TO-DO
+    public void testCreateBook() throws Exception {
+        // Arrange
+        int bookId = 1;
+        int quantity = 10;
+        
+        Book book = new Book(bookId, "1984", "George Orwell", 1949);
+        doNothing().when(bookCatalogService).addBook(book);
+    
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/catalog/book/{quantity}", quantity)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"id\":1,\"title\":\"1984\",\"author\":\"George Orwell\",\"publicationYear\":1949}"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(bookId))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("1984"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.publicationYear").value(1949));
+        
+                verify(bookCatalogService, times(1)).addBook(book);
+    }
+
+    @Test //TO-DO
+    public void testUpdateBook() throws Exception {
+        // Arrange
+        Book book = new Book(1, "1984", "George Orwell", 1949);
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.put("/catalog/book/{id}", book.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":1,\"title\":\"Updated Book\"}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(book.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Updated Book"));
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+        // Arrange
+        int bookId = 1;
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.delete("/catalog/book/{id}", bookId))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(bookCatalogService, times(1)).deleteBook(bookId);
     }
 }
